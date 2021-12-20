d7f8bc97b659a71c19d7646013cc2c53bc620f36
Gogul <gogul_nath.veeraraghavan@dhl.com>
2021-12-20 12:44:47 +0530

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# dempapp
+
+Demp config loader
+
+## Installation
+
+```sh
+npm install dempapp
+```
+
+## Usage
+
+```js
+import { multiply } from "dempapp";
+
+// ...
+
+const result = await multiply(3, 7);
+```
+
+## Contributing
+
+See the [contributing guide](CONTRIBUTING.md) to learn how to contribute to the repository and the development workflow.
+
+## License
+
+MIT
---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'dempapp' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const Dempapp = NativeModules.Dempapp
+  ? NativeModules.Dempapp
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return Dempapp.multiply(a, b);
+}
