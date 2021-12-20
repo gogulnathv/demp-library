da888d0621bb5fca348186e1ea17a32accd3c120
Gogul <gogul_nath.veeraraghavan@dhl.com>
2021-12-20 14:02:45 +0530

    Events

--- src/index.tsx
+++ src/index.tsx
@@ -10,23 +10,27 @@ const LINKING_ERROR =
 const Dempapp = NativeModules.Dempapp
   ? NativeModules.Dempapp
   : new Proxy(
-      {},
-      {
-        get() {
-          throw new Error(LINKING_ERROR);
-        },
-      }
-    );
-export const DempConfig:{result?:number} = {};
+    {},
+    {
+      get() {
+        throw new Error(LINKING_ERROR);
+      },
+    }
+  );
+export const DempConfig: { result?: number, ready: boolean } = { ready: false };
 export function multiply(a: number, b: number): Promise<number> {
   return Dempapp.multiply(a, b);
 }
-((a:number,b:number)=>{
-  multiply(a, b).then(result=>{
+((a: number, b: number) => {
+  multiply(a, b).then(result => {
     console.log(result);
-    DempConfig['result']=result;
+    DempConfig['result'] = result;
+    DempConfig['ready'] = true;
+    const EventEmitter = require('events')
+    const eventEmitter = new EventEmitter()
+    eventEmitter.emit("DempConfig-Ready")
   })
-})(33,23);
+})(33, 23);
 // export function init(){
 //   DEMPAPP.init(Dempapp)
 // }
